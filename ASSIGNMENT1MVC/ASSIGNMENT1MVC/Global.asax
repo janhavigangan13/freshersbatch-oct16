1a7017003fbf5f82e53901d0dc093dccd7ad4c50
janhavigangan13 <janhavigangan07@gmail.com>
2022-08-22 22:25:23 +0530

    Uploading MVCAssignment1

---
+++ ASSIGNMENT1MVC/ASSIGNMENT1MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASSIGNMENT1MVC.MvcApplication" Language="C#" %>
---
+++ Assignment1_3MVC/Assignment1_3MVC/Controllers/FootballController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Assignment1_3MVC.Models;
+
+namespace Assignment1_3MVC.Controllers
+{
+    public class FootballController : Controller
+    {
+        
+        public ActionResult DisplayAllDetails()
+        {
+           FootballContext db = new FootballContext();
+            return View(db.FootBallLeagues.ToList());
+        }
+        public ActionResult WinningDeatils()
+        {
+            FootballContext db = new FootballContext();
+            IEnumerable<FootBallLeague> query = db.FootBallLeagues.ToList().Where(result => result.Status1 == "Win");
+            return View(query);
+        }
+    }
+}
